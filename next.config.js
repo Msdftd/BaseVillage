bb526d991bed51a87535da30c1c1d3fdd08bae38
Scient <89262583+Msdftd@users.noreply.github.com>
2026-03-06 04:36:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/.well-known/farcaster.json",
-        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/019cbf6b-1623-bba2-28dd-2e1482b1b853",
-        permanent: false,
-      },
-    ];
-  },
-};
+const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
